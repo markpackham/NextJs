f791655bb46cbe354c38412015b5c2d3b34c9c35
Mark Packham <markpackham1@gmail.com>
2018-01-24 15:18:33 +0000

    commit installation of react, next and react-dom

---
+++ .next/dist/pages/about.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("react");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var _jsxFileName = "E:\\xampp\\htdocs\\NextJs\\pages\\about.js?entry";
+
+exports.default = function () {
+  return _react2.default.createElement("div", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 1
+    }
+  }, "About BitzPrices");
+};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInBhZ2VzXFxhYm91dC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7QUFBQTs7Ozs7Ozs7a0JBQWUsWUFBQTt5QkFBTSxjQUFBOztnQkFBQTtrQkFBQTtBQUFBO0FBQUEsR0FBQSxFQUFOLEFBQU07QUFBckIiLCJmaWxlIjoiYWJvdXQuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiRToveGFtcHAvaHRkb2NzL05leHRKcyJ9
